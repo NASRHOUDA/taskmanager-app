1e0ad787a0f69b98bc53bb50e13738413c47d072
NASRHOUDA <houdanasr414@gmail.com>
2026-07-13 00:12:35 +0100

    fix: add explicit @grpc/grpc-js dependency + enable OTel diagnostic logging

--- backend/tracing.js
+++ backend/tracing.js
@@ -1,3 +1,6 @@
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
