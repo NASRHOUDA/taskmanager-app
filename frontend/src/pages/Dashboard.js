77267d62ca3936cb0dde2f4c687537ba57073d13
NASRHOUDA <houdanasr414@gmail.com>
2026-07-15 22:47:46 +0100

    33445

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -296,7 +296,8 @@ function Dashboard() {
           value={filter.priority}
           onChange={(e) => setFilter({ ...filter, priority: e.target.value })}
         >
-          <option value="">Priorities houda
+          <option value="">Priorities houda8
+          
  </option>
           <option value="low">🟢 low </option>
           <option value="medium">🟡 Medium  </option>
