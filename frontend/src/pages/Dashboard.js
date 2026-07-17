fa559df4b9e8b994911e277a8e4226058b580ff1
NASRHOUDA <houdanasr414@gmail.com>
2026-07-17 21:09:12 +0100

    modify dahsboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -296,7 +296,7 @@ function Dashboard() {
           value={filter.priority}
           onChange={(e) => setFilter({ ...filter, priority: e.target.value })}
         >
-          <option value="">Priorities 
+          <option value="">Priorities test123
           
  </option>
           <option value="low">🟢 low </option>
